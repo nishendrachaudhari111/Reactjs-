e865252d114e4fc34aa76bc50654a579e723b0d2
Nishendra Chaudhari <149066940+nishendrachaudhari111@users.noreply.github.com>
2024-05-01 13:34:07 +0530

    BlogCard.tsx

---
+++ eCommerceWeb/src/components/BlogCard.tsx
@@ -0,0 +1,27 @@
+interface propsType {
+  img: string;
+  title: string;
+  comment: number;
+  date: string;
+}
+export const BlogCard: React.FC<propsType> = ({
+  img,
+  title,
+  date,
+  comment,
+}) => {
+  return (
+    <div className="space-y-4 ">
+      <img
+        className="rounded lg hover:scale-105 transition-transform"
+        src={img}
+        alt="post"
+      />
+      <div className="text-accent font-medium ">
+        <span>{date} / </span>
+        <span>{comment}</span>
+      </div>
+      <h3 className="font-bold text-xl">{title}</h3>
+    </div>
+  );
+};
