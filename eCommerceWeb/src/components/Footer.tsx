41dac2331b90f957216044295b5a6ead761d1caa
Nishendra Chaudhari <149066940+nishendrachaudhari111@users.noreply.github.com>
2024-05-01 13:36:43 +0530

    Footer.tsx

---
+++ eCommerceWeb/src/components/Footer.tsx
@@ -0,0 +1,11 @@
+
+
+export const Footer = () => {
+  return (
+    <div className="border-t border-gray-300 py-4">
+        <div className="container text-center text-gray-500 py-4">
+            &copy; Copyright @The Brave Coders 2024
+        </div>
+    </div>
+  )
+}
