4f5fd375f5bd242cc0d1cfb8f7a6a3e75ec90aaa
Nishendra Chaudhari <149066940+nishendrachaudhari111@users.noreply.github.com>
2024-05-01 13:30:55 +0530

    App.tsx

--- eCommerceWeb/src/App.tsx
+++ eCommerceWeb/src/App.tsx
@@ -1,7 +1,14 @@
 
+import BannerSection from './components/BannerSection'
+import Blog from './components/Blog'
 import { Category } from './components/Category'
+import { FeatureSection } from './components/FeatureSection'
+import { FeatureSectionBreackfast } from './components/FeatureSectionBreackfast'
+import { FeatureSectionFruits } from './components/FeatureSectionFruits'
+import { Footer } from './components/Footer'
 import { Hero } from './components/Hero'
 import Navbar from './components/Navbar'
+import Newsletter from './components/NewsLatter'
 
 const App = () => {
   return (
@@ -9,6 +16,13 @@ const App = () => {
       <Navbar />
       <Hero />
       <Category />
+      <FeatureSectionFruits />
+      <FeatureSectionBreackfast />
+      <BannerSection />
+      <Blog />
+      <Newsletter />
+      <FeatureSection />
+      <Footer />
     </main>
   )
 }
